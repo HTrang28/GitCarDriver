870ce1ba036a31847cbd3c09d779f495240d5dae
NguyenCanhThanh <48113236+NguyenCanhThanh@users.noreply.github.com>
2020-12-01 12:53:14 +0700

    Delete .rtcar.ko.cmd

--- src/drivers/.rtcar.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/catkin_ws/src/picar/src/drivers/rtcar.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/ubuntu/catkin_ws/src/picar/src/drivers/rtcar.ko /home/ubuntu/catkin_ws/src/picar/src/drivers/rtcar.o /home/ubuntu/catkin_ws/src/picar/src/drivers/rtcar.mod.o
